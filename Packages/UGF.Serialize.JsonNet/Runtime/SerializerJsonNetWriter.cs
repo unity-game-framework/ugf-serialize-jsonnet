bea0b5b29077b9ee3c8bb71b9644cfb1acc1c1f2
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-15 22:21:37 +0300

    Add JsonNet serializers and tests

---
+++ Packages/UGF.Serialize.JsonNet/Runtime/SerializerJsonNetWriter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace UGF.Serialize.JsonNet.Runtime
+{
+    public class SerializerJsonNetWriter : JsonTextWriter
+    {
+        public TextWriter Writer { get; }
+
+        public SerializerJsonNetWriter(TextWriter writer) : base(writer)
+        {
+            Writer = writer;
+        }
+
+        public override string ToString()
+        {
+            return Writer.ToString();
+        }
+    }
+}
