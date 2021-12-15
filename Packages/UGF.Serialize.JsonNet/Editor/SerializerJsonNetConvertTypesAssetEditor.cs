1bc134fa31e25c30c10c2023e87bd5714c3960f6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-15 23:01:00 +0300

    Update SerializerJsonNetConvertTypesAssetEditor.cs

--- Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypesAssetEditor.cs
+++ Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypesAssetEditor.cs
@@ -7,14 +7,15 @@ namespace UGF.Serialize.JsonNet.Editor
     [CustomEditor(typeof(SerializerJsonNetConvertTypesAsset), true)]
     internal class SerializerJsonNetConvertTypesAssetEditor : SerializerJsonNetConvertNamesAssetEditor
     {
+        private SerializedProperty m_propertyAllowAllTypes;
         private ReorderableListDrawer m_listTypes;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
+            m_propertyAllowAllTypes = serializedObject.FindProperty("m_allowAllTypes");
             m_listTypes = new ReorderableListDrawer(serializedObject.FindProperty("m_types"));
-
             m_listTypes.Enable();
         }
 
@@ -28,6 +29,8 @@ namespace UGF.Serialize.JsonNet.Editor
         protected override void OnDrawGUILayout()
         {
             base.OnDrawGUILayout();
+
+            EditorGUILayout.PropertyField(m_propertyAllowAllTypes);
 
             m_listTypes.DrawGUILayout();
         }
