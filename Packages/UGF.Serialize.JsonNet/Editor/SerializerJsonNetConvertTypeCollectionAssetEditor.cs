6125072dbdaed0a6e46e93410f99876536e55987
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-16 16:59:02 +0300

    Add SerializerJsonNetConvertTypeCollectionAssetEditor

---
+++ Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypeCollectionAssetEditor.cs
@@ -0,0 +1,34 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Serialize.JsonNet.Runtime;
+using UnityEditor;
+
+namespace UGF.Serialize.JsonNet.Editor
+{
+    [CustomEditor(typeof(SerializerJsonNetConvertTypeCollectionAsset), true)]
+    internal class SerializerJsonNetConvertTypeCollectionAssetEditor : UnityEditor.Editor
+    {
+        private SerializerJsonNetConvertTypeCollectionListDrawer m_listTypes;
+
+        private void OnEnable()
+        {
+            m_listTypes = new SerializerJsonNetConvertTypeCollectionListDrawer(serializedObject.FindProperty("m_types"));
+            m_listTypes.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listTypes.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listTypes.DrawGUILayout();
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypeCollectionDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Serialize.JsonNet.Editor
+{
+    internal class SerializerJsonNetConvertTypeCollectionListDrawer : ReorderableListDrawer
+    {
+        public SerializerJsonNetConvertTypeCollectionListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty propertyId = serializedProperty.FindPropertyRelative("m_id");
+            SerializedProperty propertyType = serializedProperty.FindPropertyRelative("m_type.m_value");
+
+            EditorGUI.LabelField(position, $"{propertyId.stringValue}:{propertyType.stringValue}");
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override bool OnElementHasVisibleChildren(SerializedProperty serializedProperty)
+        {
+            return false;
+        }
+    }
+}
