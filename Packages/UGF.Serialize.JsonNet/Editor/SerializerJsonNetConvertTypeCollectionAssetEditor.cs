6914fad48ff1e9dcf81b4280715e123023680345
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-02-08 18:21:57 +0300

    Update SerializerJsonNetConvertTypeCollectionAssetEditor.cs

--- Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypeCollectionAssetEditor.cs
+++ Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetConvertTypeCollectionAssetEditor.cs
@@ -1,12 +1,16 @@
-﻿using UGF.EditorTools.Editor.IMGUI;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UGF.EditorTools.Editor.IMGUI;
 using UGF.EditorTools.Editor.IMGUI.Scopes;
 using UGF.Serialize.JsonNet.Runtime;
 using UnityEditor;
+using UnityEngine;
 
 namespace UGF.Serialize.JsonNet.Editor
 {
     [CustomEditor(typeof(SerializerJsonNetConvertTypeCollectionAsset), true)]
-    internal class SerializerJsonNetConvertTypeCollectionAssetEditor : UnityEditor.Editor
+    public class SerializerJsonNetConvertTypeCollectionAssetEditor : UnityEditor.Editor
     {
         private SerializerJsonNetConvertTypeCollectionListDrawer m_listTypes;
 
@@ -29,6 +33,71 @@ namespace UGF.Serialize.JsonNet.Editor
 
                 m_listTypes.DrawGUILayout();
             }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Collect"))
+                {
+                    OnCollect();
+                }
+
+                EditorGUILayout.Space();
+            }
+        }
+
+        protected virtual void OnCollectTypes(IDictionary<string, Type> types)
+        {
+            TypeCache.TypeCollection collection = TypeCache.GetTypesWithAttribute<SerializerJsonNetTypeAttribute>();
+
+            foreach (Type type in collection)
+            {
+                var attribute = type.GetCustomAttribute<SerializerJsonNetTypeAttribute>();
+
+                if (!types.ContainsKey(attribute.Id))
+                {
+                    types.Add(attribute.Id, type);
+                }
+            }
+        }
+
+        private void OnCollect()
+        {
+            SerializedProperty propertyArray = m_listTypes.SerializedProperty;
+            var types = new Dictionary<string, Type>();
+
+            for (int i = 0; i < propertyArray.arraySize; i++)
+            {
+                SerializedProperty propertyElement = propertyArray.GetArrayElementAtIndex(i);
+                SerializedProperty propertyId = propertyElement.FindPropertyRelative("m_id");
+                SerializedProperty propertyValue = propertyElement.FindPropertyRelative("m_type.m_value");
+
+                var type = Type.GetType(propertyValue.stringValue);
+
+                if (type != null)
+                {
+                    types.Add(propertyId.stringValue, type);
+                }
+            }
+
+            OnCollectTypes(types);
+
+            propertyArray.ClearArray();
+
+            foreach ((string key, Type value) in types)
+            {
+                propertyArray.InsertArrayElementAtIndex(propertyArray.arraySize);
+
+                SerializedProperty propertyElement = propertyArray.GetArrayElementAtIndex(propertyArray.arraySize - 1);
+                SerializedProperty propertyId = propertyElement.FindPropertyRelative("m_id");
+                SerializedProperty propertyValue = propertyElement.FindPropertyRelative("m_type.m_value");
+
+                propertyId.stringValue = key;
+                propertyValue.stringValue = value.AssemblyQualifiedName;
+            }
+
+            propertyArray.serializedObject.ApplyModifiedProperties();
         }
     }
 }
