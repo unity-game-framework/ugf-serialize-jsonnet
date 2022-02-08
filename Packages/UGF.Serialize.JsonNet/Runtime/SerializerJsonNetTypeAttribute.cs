c2ac848c971d9534c1144acc7e74bdf6288072f2
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-02-08 18:03:26 +0300

    Add SerializerJsonNetTypeAttribute

---
+++ Packages/UGF.Serialize.JsonNet/Runtime/SerializerJsonNetTypeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UGF.Serialize.JsonNet.Runtime
+{
+    public class SerializerJsonNetTypeAttribute : Attribute
+    {
+        public string Id { get; }
+
+        public SerializerJsonNetTypeAttribute()
+        {
+            Id = Guid.NewGuid().ToString("N");
+        }
+
+        public SerializerJsonNetTypeAttribute(string id)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            Id = id;
+        }
+    }
+}
