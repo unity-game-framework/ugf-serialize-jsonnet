4257e70bf0ebce35df17c85d63844c16a17eedc3
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-03-20 22:23:20 +0300

    Change tests

--- Assets/UGF.Serialize.JsonNet.Runtime.Tests/TestSerializedTypes.cs
+++ Assets/UGF.Serialize.JsonNet.Runtime.Tests/TestSerializedTypes.cs
@@ -1,7 +1,30 @@
-﻿namespace UGF.Serialize.JsonNet.Runtime.Tests
+﻿using System;
+using NUnit.Framework;
+
+namespace UGF.Serialize.JsonNet.Runtime.Tests
 {
     public class TestSerializedTypes
     {
+        [SerializerJsonNetType]
+        private class Target
+        {
+        }
+
+        [Test]
+        public void NotVisible()
+        {
+            var type1 = Type.GetType("UGF.Serialize.JsonNet.Runtime.Tests.TestCustomSerializer+Target1, UGF.Serialize.JsonNet.Runtime.Tests, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+            var type2 = Type.GetType("UGF.Serialize.JsonNet.Runtime.Tests.TestSerializedTypes+Target, UGF.Serialize.JsonNet.Runtime.Tests, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+            Assert.NotNull(type1);
+            Assert.NotNull(type2);
+
+            object result1 = Activator.CreateInstance(type1);
+            object result2 = Activator.CreateInstance(type2);
+
+            Assert.NotNull(result1);
+            Assert.NotNull(result2);
+        }
     }
 
     [SerializerJsonNetType("c8703a9b7b514209a05c64835ab4d0a5")]
@@ -16,6 +39,11 @@
 
     [SerializerJsonNetType("882939dac4ed40c1a3789174e0e3e2c9")]
     public class TestSerialized3
+    {
+    }
+
+    [SerializerJsonNetType]
+    public abstract class TestSerializedAbstract
     {
     }
 }
