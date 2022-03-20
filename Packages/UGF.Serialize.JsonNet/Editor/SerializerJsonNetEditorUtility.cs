76461e20c90a3365d51ad493580dc0e6cfc8be59
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-03-20 22:22:55 +0300

    Add SerializerJsonNetEditorUtility

---
+++ Packages/UGF.Serialize.JsonNet/Editor/SerializerJsonNetEditorUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace UGF.Serialize.JsonNet.Editor
+{
+    public static class SerializerJsonNetEditorUtility
+    {
+        public static bool IsValidSerializableType(Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            return !type.IsAbstract
+                   && !type.IsArray
+                   && !type.IsEnum
+                   && !type.IsInterface
+                   && !type.IsGenericTypeDefinition
+                   && !type.IsGenericType
+                   && type.GetConstructor(Type.EmptyTypes) != null
+                   && type.GetCustomAttribute<CompilerGeneratedAttribute>() == null;
+        }
+    }
+}
