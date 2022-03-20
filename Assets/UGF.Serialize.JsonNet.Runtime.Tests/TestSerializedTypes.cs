6aa3346370fe6745425f4041e3a7d69763c03f99
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-03-20 21:41:34 +0300

    Change tests

---
+++ Assets/UGF.Serialize.JsonNet.Runtime.Tests/TestSerializedTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace UGF.Serialize.JsonNet.Runtime.Tests
+{
+    public class TestSerializedTypes
+    {
+    }
+
+    [SerializerJsonNetType("c8703a9b7b514209a05c64835ab4d0a5")]
+    public class TestSerialized1
+    {
+    }
+
+    [SerializerJsonNetType("8bb2613576f340d68d4a304aad5a0948")]
+    public class TestSerialized2
+    {
+    }
+
+    [SerializerJsonNetType("882939dac4ed40c1a3789174e0e3e2c9")]
+    public class TestSerialized3
+    {
+    }
+}
